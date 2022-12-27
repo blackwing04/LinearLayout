f8df50e75fd992a3a4f2d0d50590068172135bd4
blackwing <hh4654075hh@gmail.com>
2022-12-27 20:50:34 +0800

    用LinearLayout排列元件 -完成 增加檢查分數等等

--- app/src/main/java/com/example/score/MainActivity.kt
+++ app/src/main/java/com/example/score/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.score
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.databinding.DataBindingUtil
+import com.example.score.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.ivChPass.visibility =View.INVISIBLE
+        binding.ivEhPass.visibility =View.INVISIBLE
+        binding.ivMathPass.visibility =View.INVISIBLE
+        binding.btCk.setOnClickListener {
+            check(binding.etCH.text.toString().toInt(),binding.ivChPass)
+            check(binding.etEH.text.toString().toInt(),binding.ivEhPass)
+            check(binding.etMath.text.toString().toInt(),binding.ivMathPass)
+            binding.textView.requestFocus()
+        }
+        binding.etCH.setOnFocusChangeListener { view, b ->
+            if (b) binding.ivChPass.visibility =View.INVISIBLE
+        }
+        binding.etEH.setOnFocusChangeListener { view, b ->
+            if (b) binding.ivEhPass.visibility =View.INVISIBLE
+        }
+        binding.etMath.setOnFocusChangeListener { view, b ->
+            if (b) binding.ivMathPass.visibility =View.INVISIBLE
+        }
+    }
+    private fun check(Score:Int,iv:ImageView){
+        if (Score>=60){
+            iv.setImageResource(R.drawable.ic_ok)
+        }
+        else{
+            iv.setImageResource(R.drawable.ic_fail)
+        }
+        iv.visibility =View.VISIBLE
     }
 }
